41e4dc4e9712d1331cda00ae648e1b2a2d8d7353
Kamenetsky <kamenetsky007@gmail.com>
2019-03-08 20:27:02 +0500

    2

--- lesson3/src/Main.java
+++ lesson3/src/Main.java
@@ -2,10 +2,28 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> a = new ArrayList<>();
-        a.add(1);
-        a.add(2);
-        a.add(3);
+        ArrayList<String> array = new ArrayList<>();
+
+        array.add("Стол");
+        array.add("Стул");
+        array.add("Скатерть");
+        array.add("Стол");
+        array.add("Скатерть");
+        array.add("Стул");
+        array.add("Скатерть");
+        array.add("Книга");
+        array.add("Стул");
+        array.add("Стол");
+        array.add("Папка");
+        array.add("Скатерть");
+        array.add("Стул");
+        array.add("Скалка");
+        array.add("Папка");
+        array.add("Папка");
+        array.add("Кружка");
+        array.add("Папка");
+        array.add("Папка");
+        array.add("Папка");
 
     }
 }
--- lesson3/src/Unique.java
+++ lesson3/src/Unique.java
@@ -1,30 +1,28 @@
 import java.util.*;
 
 public class Unique {
-    ArrayList<String> array = new ArrayList<>();
+    ArrayList<String> array1 = new ArrayList<>();
 
 
-    array.add("Стол");
-    array.add("Стул");
-    array.add("Скатерть");
-    array.add("Стол");
-    array.add("Скатерть");
-    array.add("Стул");
-    array.add("Скатерть");
-    array.add("Книга");
-    array.add("Стул");
-    array.add("Стол");
-    array.add("Папка");
-    array.add("Скатерть");
-    array.add("Стул");
-    array.add("Скалка");
-    array.add("Папка");
-    array.add("Папка");
-    array.add("Кружка");
-    array.add("Папка");
-    array.add("Папка");
-    array.add("Папка");
+    array1.add("Стол");
+    array1.add("Стул");
+    array1.add("Скатерть");
+    array1.add("Стол");
+    array1.add("Скатерть");
+    array1.add("Стул");
+    array1.add("Скатерть");
+    array1.add("Книга");
+    array1.add("Стул");
+    array1.add("Стол");
+    array1.add("Папка");
+    array1.add("Скатерть");
+    array1.add("Стул");
+    array1.add("Скалка");
+    array1.add("Папка");
+    array1.add("Папка");
+    array1.add("Кружка");
+    array1.add("Папка");
+    array1.add("Папка");
+    array1.add("Папка");
 
-
-    }
 }
