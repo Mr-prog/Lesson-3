302a3c153bcc7c989c0cc88c8708620695f57605
Kamenetsky <kamenetsky007@gmail.com>
2019-03-08 17:02:25 +0500

    0.1

---
+++ lesson3/src/Unique.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+public class Unique {
+    ArrayList<String> array = new ArrayList<>();
+
+
+    array.add("Стол");
+    array.add("Стул");
+    array.add("Скатерть");
+    array.add("Стол");
+    array.add("Скатерть");
+    array.add("Стул");
+    array.add("Скатерть");
+    array.add("Книга");
+    array.add("Стул");
+    array.add("Стол");
+    array.add("Папка");
+    array.add("Скатерть");
+    array.add("Стул");
+    array.add("Скалка");
+    array.add("Папка");
+    array.add("Папка");
+    array.add("Кружка");
+    array.add("Папка");
+    array.add("Папка");
+    array.add("Папка");
+
+
+    }
+}
